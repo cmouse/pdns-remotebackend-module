bda85d04e429f5b6b5e7db8f5b4f042bbc5a2444
Aki Tuomi <cmouse@desteem.org>
2012-07-18 16:16:35 +0300

    Fixed code to actually work for a simple domain

--- socketbackend.cc
+++ socketbackend.cc
@@ -2,6 +2,153 @@
 
 static const char *kBackendId = "[SocketBackend]";
 
+SocketBackend::SocketBackend(const std::string &suffix)
+{
+      setArgPrefix("socket"+suffix);
+      this->connector = Socketbackend::Connector::build(getArg("connection-string"));
+      this->d_dnssec = mustDo("dnssec");
+      d_result = d_position = NULL;
+      L<<Logger::Info<<"Created SocketBackend"<<endl;
+}
+
+SocketBackend::~SocketBackend() {
+     if (connector != NULL) delete connector;
+     if (d_result != NULL) json_delete(d_result);
+}
+
+void SocketBackend::lookup(const QType &qtype, const std::string &qdomain, DNSPacket *pkt_p, int zoneId)
+{
+      struct JsonNode *query, *args;
+
+      L<<Logger::Info<<"Running lookup for "<<qdomain<<endl;
+
+      if (d_result != NULL) {
+          throw new AhuException("Lookup while get running");
+      }
+
+      query = json_mkobject();
+      args = json_mkobject();
+
+      json_append_member(query, "method", json_mkstring("lookup"));
+      json_append_member(args, "qtype", json_mkstring(qtype.getName().c_str()));
+      json_append_member(args, "qname", json_mkstring(qdomain.c_str()));
+      if (pkt_p != NULL) {
+        json_append_member(args, "remote", json_mkstring(pkt_p->getRemote().c_str()));
+        json_append_member(args, "local", json_mkstring(pkt_p->getLocal().c_str()));
+        json_append_member(args, "real-remote", json_mkstring(pkt_p->getRealRemote().toString().c_str()));
+      }
+      json_append_member(query, "parameters", args);
+
+      if (connector->query(query, &d_result) == false) {
+         L<<Logger::Error<<"Failed to query "<<connector->getConnectorName()<<endl;
+      }
+      json_delete(query);
+
+      if (d_result == NULL) return; // it failed. 
+
+      d_position = json_find_member(d_result, "result"); // should be an array...
+      // check for "false"
+      if (d_position->tag == JSON_BOOL && d_position->bool_ == false) {
+         // call failed
+          json_delete(d_result);
+          d_result = NULL;
+          return;
+      }
+      if (d_position->tag != JSON_ARRAY) {
+         json_delete(d_result); 
+         d_result = NULL;
+         throw new AhuException("Expected result array - got something else");
+      }
+
+      // set it to first element
+      d_position = json_first_child(d_position);
+}
+bool SocketBackend::get(DNSResourceRecord &rr) {
+      if (d_result == NULL) return false;
+      JsonNode *ptr;
+
+      try {
+      // parse rr
+      rr.qtype = json_value("qtype",d_position)->string_;
+      rr.qname = json_value("qname",d_position)->string_;
+      rr.content = json_value("content",d_position)->string_;
+      } catch (...) {
+        L<<Logger::Error<<"Cannot parse result line a"<<endl;
+      }
+
+      try {
+      rr.ttl = static_cast<int>(json_value("ttl",d_position)->number_);
+      rr.domain_id = static_cast<int>(json_value("domain_id",d_position)->number_);
+      rr.priority = static_cast<int>(json_value("priority",d_position)->number_);
+      } catch (...) {
+        L<<Logger::Error<<"Cannot parse result line b"<<endl;
+      }
+
+      try {
+      if ((ptr = json_value("auth", d_position, !d_dnssec)) != NULL)
+        rr.auth = static_cast<int>(ptr->number_) != 0;
+      else
+        rr.auth = 1;
+
+      if ((ptr = json_value("scope_mask", d_position, true)) != NULL)
+        rr.scopeMask = lexical_cast<uint8_t>(ptr->string_);
+
+      } catch (...) {
+        L<<Logger::Error<<"Cannot parse result line c"<<endl;
+      }
+
+      d_position = d_position->next;
+
+      if (d_position == NULL)
+      {
+         json_delete(d_result);
+         d_result = NULL;
+      }
+
+      return true;
+}
+
+bool SocketBackend::list(const std::string &target, int domain_id) {
+      struct JsonNode *query, *args;
+      if (d_result != NULL) {
+          throw new AhuException("Lookup while get running");
+      }
+
+      query = json_mkobject();
+      args = json_mkobject();
+
+      json_append_member(query, "method", json_mkstring("list"));
+      json_append_member(args, "target", json_mkstring(target.c_str()));
+      json_append_member(args, "domain_id", json_mknumber(domain_id));
+
+      json_append_member(query, "parameters", args);
+
+      if (connector->query(query, &d_result) == false) {
+         L<<Logger::Error<<"Failed to query "<<connector->getConnectorName()<<endl;
+      }
+      json_delete(query);
+
+      if (d_result == NULL) return false;
+
+      d_position = json_find_member(d_result, "result");
+
+      // check for "false"
+      if (d_position->tag == JSON_BOOL && d_position->bool_ == false) {
+         // call failed
+          json_delete(d_result);
+          d_result = NULL;
+          return false;
+      }
+      if (d_position->tag != JSON_ARRAY) {
+         json_delete(d_result); // this cannot be NULL anymore.
+         d_result = NULL;
+         throw new AhuException("Expected result array - got something else");
+      }
+
+      return true;
+};
+
+
 DNSBackend *SocketBackend::maker()
 {
    try {
--- socketbackend.hh
+++ socketbackend.hh
@@ -33,125 +33,22 @@ private:
 class SocketBackend : public DNSBackend 
 {
   public:
-  SocketBackend(const std::string &suffix="")
-  {
-      this->connector = Socketbackend::Connector::build(getArg("connection-string"));
-      this->d_dnssec = mustDo("dnssec");
-  }
+  SocketBackend(const std::string &suffix="");
+  ~SocketBackend(); 
 
-  void lookup(const QType &qtype, const std::string &qdomain, DNSPacket *pkt_p=0, int zoneId=-1)
-  {
-      struct JsonNode *query, *args;
-      if (d_result != NULL) {
-          throw new SBException("Lookup while get running");
-      }
+  void lookup(const QType &qtype, const std::string &qdomain, DNSPacket *pkt_p=0, int zoneId=-1);
+  bool get(DNSResourceRecord &rr); 
+  bool list(const std::string &target, int domain_id); 
 
-      query = json_mkobject();
-      args = json_mkobject();
+  // unimplemented
+/*  virtual bool getSOA(const std::string &name, SOAData &soadata, DNSPacket *p=0) { return false; };
+  virtual bool getDomainMetadata(const std::string& name, const std::string& kind, std::vector<std::string>& meta) { return false; };
+  virtual bool getDomainKeys(const std::string& name, unsigned int kind, std::vector<DNSBackend::KeyData>& keys) { return false; };
+  virtual bool getTSIGKey(const std::string& name, std::string* algorithm, std::string* content) { return false; };
+  virtual bool getBeforeAndAfterNamesAbsolute(uint32_t id, const std::string& qname, std::string& unhashed, std::string& before, std::string& after) { return false; };
+  virtual bool getBeforeAndAfterNames(uint32_t id, const std::string& zonename, const std::string& qname, std::string& before, std::string& after) { return false; };
 
-      json_append_member(query, "method", json_mkstring("lookup"));
-      json_append_member(args, "qtype", json_mkstring(qtype.getName().c_str()));
-      json_append_member(args, "qname", json_mkstring(qdomain.c_str()));
-      if (pkt_p != NULL) {
-        json_append_member(args, "remote", json_mkstring(pkt_p->getRemote().c_str()));
-        json_append_member(args, "local", json_mkstring(pkt_p->getLocal().c_str()));
-        json_append_member(args, "real-remote", json_mkstring(pkt_p->getRealRemote().toString().c_str()));
-      }
-      json_append_member(query, "parameters", args);
-
-      if (connector->query(query, &d_result) == false) {
-         L<<Logger::Error<<"Failed to query "<<connector->getConnectorName()<<endl;
-      }
-      json_delete(query);
-
-      d_position = json_find_member(d_result, "result"); // should be an array... 
-      // check for "false"
-      if (d_position->tag == JSON_BOOL && d_position->bool_ == false) {
-         // call failed
-          json_delete(d_result);
-          return;
-      }
-
-      if (d_position->tag != JSON_ARRAY) {  
-         json_delete(d_result); // this cannot be NULL anymore.
-         throw new SBException("Expected result array - got something else");
-      }
-  }
-
-  bool get(DNSResourceRecord &rr) { 
-      if (d_result == NULL) return false;
-
-      // parse rr
-      rr.qtype = json_value("qtype",d_position)->string_;
-      rr.qname = json_value("qname",d_position)->string_;
-      rr.ttl = static_cast<int>(json_value("ttl",d_position)->number_);
-      rr.domain_id = static_cast<int>(json_value("domain_id",d_position)->number_);
-      rr.priority = static_cast<int>(json_value("priority",d_position)->number_); 
-
-      if (json_value("auth", d_position, d_dnssec))
-        rr.auth = static_cast<int>(json_value("auth", d_position)->number_) != 0;
-      else
-        rr.auth = 1;
-
-      if (json_value("scope_mask", d_position, true))
-        rr.scopeMask = lexical_cast<uint8_t>(json_value("auth", d_position)->string_);
-
-      d_position = d_position->next;
-
-      if (d_position == NULL)
-      {
-         json_delete(d_result);
-         d_result = NULL;
-      }
- 
-      return true;
-  }
-
-  bool list(const std::string &target, int domain_id) { 
-      struct JsonNode *query, *args;
-      if (d_result != NULL) {
-          throw new SBException("Lookup while get running");
-      }
-
-      query = json_mkobject();
-      args = json_mkobject();
-
-      json_append_member(query, "method", json_mkstring("list"));
-      json_append_member(args, "target", json_mkstring(target.c_str()));
-      json_append_member(args, "domain_id", json_mknumber(domain_id));
-
-      json_append_member(query, "parameters", args);
-
-      if (connector->query(query, &d_result) == false) {
-         L<<Logger::Error<<"Failed to query "<<connector->getConnectorName()<<endl;
-      }
-      json_delete(query);
-
-      d_position = json_find_member(d_result, "result"); 
-
-      // check for "false"
-      if (d_position->tag == JSON_BOOL && d_position->bool_ == false) {
-         // call failed
-          json_delete(d_result);
-          return false;
-      }
-      if (d_position->tag != JSON_ARRAY) {
-         json_delete(d_result); // this cannot be NULL anymore.
-         throw new SBException("Expected result array - got something else");
-      }
-
-      return true;
-  };
-
-  ~SocketBackend() {};
-  bool getSOA(const std::string &name, SOAData &soadata, DNSPacket *p=0) { return false; };
-  bool getDomainMetadata(const std::string& name, const std::string& kind, std::vector<std::string>& meta) { return false; };
-  bool getDomainKeys(const std::string& name, unsigned int kind, std::vector<DNSBackend::KeyData>& keys) { return false; };
-  bool getTSIGKey(const std::string& name, std::string* algorithm, std::string* content) { return false; };
-  bool getBeforeAndAfterNamesAbsolute(uint32_t id, const std::string& qname, std::string& unhashed, std::string& before, std::string& after) { return false; };
-  bool getBeforeAndAfterNames(uint32_t id, const std::string& zonename, const std::string& qname, std::string& before, std::string& after) { return false; };
-
-  bool getDomainInfo(const std::string &domain, DomainInfo &di) { return false; };
+  virtual bool getDomainInfo(const std::string &domain, DomainInfo &di) { return false; };*/
 
   static DNSBackend *maker();
 
