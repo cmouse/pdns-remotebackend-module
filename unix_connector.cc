5e54b57730e5feb218a5441d77b4085b8bc4dd59
Aki Tuomi <cmouse@desteem.org>
2012-07-18 16:15:30 +0300

    Improved logging, made it flush socket on connect

--- unix_connector.cc
+++ unix_connector.cc
@@ -24,25 +24,45 @@ void UnixConnector::reconnect() {
   L<<Logger::Info<<getConnectorName()<<" is (re)connecting to "<<options["path"]<<endl;
 
   // check if path exists
-  if ((errno = stat(options["path"].c_str(), &buf))!=0) {
-    L<<Logger::Error<<"Cannot connect: "<<strerror(errno)<<endl;
+  if ((errno = stat(options["path"].c_str(), &buf)) < 0) {
+    L<<Logger::Error<<"Cannot stat: "<<strerror(errno)<<endl;
     return;
   }
 
-  if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-    L<<Logger::Error<<"Cannot connect: "<<strerror(errno)<<endl;
+  if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+    L<<Logger::Error<<"Cannot create socket: "<<strerror(errno)<<endl;
     return;
   }
 
   sa.sun_family = AF_UNIX;
   strncpy(sa.sun_path, options["path"].c_str(), UNIX_PATH_MAX);
 
-  if (connect(sock, reinterpret_cast<struct sockaddr*>(&sa), sizeof sa)) {
+  if (connect(sock, reinterpret_cast<struct sockaddr*>(&sa), sizeof sa) < 0) {
     L<<Logger::Error<<"Cannot connect: "<<strerror(errno)<<endl;
     return;
   }
 
   connected = true;
+ 
+  //make sure no data is left there...
+  while(1) {
+    fd_set rs;
+    struct timeval tv;
+    FD_ZERO(&rs);
+    FD_SET(sock, &rs);
+    tv.tv_sec = 0;
+    tv.tv_usec = 1000;
+    if (select(sock+1, &rs, NULL, NULL, &tv)>0) {
+       char data[1500];
+       if (FD_ISSET(sock,&rs)) {
+          if (read(sock, data, sizeof data) < 1) {
+             connected = false;
+             close(sock); 
+             break;
+          }
+       }
+    } else break;
+  }
 }
 
 };
